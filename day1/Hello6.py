eb9fa201f8087190abd06a8f49113fc1f09fff88
段維瀚 vincent tuan <vincentjava@yahoo.com.tw>
2021-12-15 21:29:35 +0800

    Update Hello6.py

--- day1/Hello6.py
+++ day1/Hello6.py
@@ -1,1 +1,7 @@
-# 讓使用者輸入半徑 r, 可以得到圓面積與球體積
+# 讓使用者輸入半徑 r (可支援小數 = 浮點數), 可以得到圓面積與球體積
+r = float(input('輸入半徑:'))
+pi = 3.14
+area = r * r * pi
+volume = 4/3 * pi * r ** 3
+print('圓面積:', area)
+print('球體積:', volume)
