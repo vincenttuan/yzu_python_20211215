be461b7fa709d85d15a5f19e457bef73cb4eb202
段維瀚 vincent tuan <vincentjava@yahoo.com.tw>
2022-01-12 19:49:47 +0800

    Add files via upload

---
+++ day5/演算法-洗牌2.py
@@ -0,0 +1,12 @@
+# 洗牌-互換位置
+import random as r
+poker = ['A', '2', '3', '4', '5', '6', '7', '8', '9',
+         '10', 'J', 'Q', 'K'] * 4
+print(poker)
+for i in range(100):
+    # 洗牌
+    x = r.randint(0, len(poker)-1)
+    y = r.randint(0, len(poker)-1)
+    poker[x], poker[y] = poker[y], poker[x]
+
+print(poker)
