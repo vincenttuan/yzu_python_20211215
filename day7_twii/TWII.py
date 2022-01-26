7dd40f6aecb3702534e2f9f32b6ad3ed02001f19
段維瀚 vincent tuan <vincentjava@yahoo.com.tw>
2022-01-26 21:29:35 +0800

    Add files via upload

--- day7_twii/TWII.py
+++ day7_twii/TWII.py
@@ -15,8 +15,16 @@ data = data.replace('"', '')
 # 將「-」變成「-1」
 data = data.replace('-', '-1')
 #print(data)
+yield_rate = 7 # 殖利率
+pe = 12 # 本益比
+pb = 1  # 股價淨值比
+yield_rate, pe, pb = map(int, input('請依序輸入 殖利率,本益比,股價淨值比（例如：7,12,1）==> ').split(','))
+print('證券代號,證券名稱,殖利率(%),股利年度,本益比,股價淨值比,財報年/季');
 for d in data.split("\r\n"):
     list = d.split(',')
     if len(list) == 8 and list[0] != '證券代號':
-        # 證券代號,證券名稱,殖利率(%),股利年度,本益比,股價淨值比,財報年/季,
-        print(list)
+        if float(list[2]) > yield_rate and \
+            float(list[4]) < pe and \
+            float(list[5]) < pb:
+            print(list)
+
