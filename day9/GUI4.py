dae1c2e69df8ba12861a4649f7a1fb5fdbd2cac0
段維瀚 vincent tuan <vincentjava@yahoo.com.tw>
2022-02-16 19:17:48 +0800

    Add files via upload

--- day9/GUI4.py
+++ day9/GUI4.py
@@ -4,26 +4,32 @@ from tkinter import messagebox
 
 '''
 +------------+
-| My Window3 |
+| My Window4 |
 +------------+
-|   Label    |
-| BTN1 BTN2  |
+|     5      |
+| Add   Exit |
 +-------------
 '''
-def show_message():
-    messagebox.showinfo('Hello', 'I Love Python ...')
+value = 0
+
+def add():
+    global value
+    value += 1
+    var.set(str(value))
 
 def win_exit():
     win.quit()
 
 if __name__ == '__main__':
     win = tkinter.Tk()  # 取得視窗物件
-    win.title('My Window 3')  # 設定視窗抬頭
+    win.title('My Window 4')  # 設定視窗抬頭
     win.geometry("300x300")  # 設定視窗尺寸大小
     # Label 元素配置
+    var = tkinter.StringVar()  # 字串參照物件
+    var.set("0")
     label = tkinter.Label(
         win,
-        text='Hello',
+        textvariable=var,
         bg='yellow',
         font=('Arial, 20'),
         width=30,
@@ -31,7 +37,7 @@ if __name__ == '__main__':
     )
     label.pack()
     # Button 元素配置
-    button1 = tkinter.Button(win, text="OK", width=10, height=2, font=('Arial', 20), command=show_message)
+    button1 = tkinter.Button(win, text="Add", width=10, height=2, font=('Arial', 20), command=add)
     button2 = tkinter.Button(win, text="Exit", width=10, height=2, font=('Arial', 20), command=win_exit)
     button1.pack(side=tkinter.LEFT)
     button2.pack(side=tkinter.RIGHT)
